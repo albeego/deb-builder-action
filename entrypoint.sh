b061674d89f2b7b078ded28c3149f39b30209f9f
Steven Leadbeater <info@ledsysconsultancy.com>
2020-07-09 10:58:50 +0100

    Refactor method for applying version number in change log to account for tags with / characters

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ VERSION=$(git tag --sort=-taggerdate --format "%(refname:lstrip=-1)" | head -1)
 echo "Latest version: $VERSION"
 
 mkdir -p build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian
-git tag --sort=-authordate --format "$INPUT_PACKAGE_NAME (%(refname:lstrip=-1)) UNRELEASED; urgency=medium%0a%0a  * %(subject)%0a%0a -- %(authorname) %(authoremail)  %(authordate:rfc2822)" > build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/changelog
+git tag --sort=-authordate \
+    --format "$INPUT_PACKAGE_NAME (%(refname:lstrip=-1)) UNRELEASED; urgency=medium%0a%0a  * %(subject)%0a%0a -- %(authorname) %(authoremail)  %(authordate:rfc2822)" \
+    > build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/changelog
 echo 9 > build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/compat
 cp -r debian/* build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/
 sed -i "s/\$VERSION/$VERSION/g" build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/control
