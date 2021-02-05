cecf124b8ec77d2f042081aa3f9d157803f4354f
steven leadbeater <stevenleadbeater@live.co.uk>
2021-02-05 10:17:18 +0000

    Make version explicitly passed in

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,16 @@ set -e
 
 cd "$INPUT_EXECUTION_PATH"
 
-VERSION_NUMBER=$(git tag --sort=-taggerdate --format "%(refname:lstrip=-1)" | head -1)
-VERSION=${VERSION_NUMBER#$INPUT_VERSION_PREFIX}
-echo "Latest version: $VERSION"
+echo "Latest version: $INPUT_VERSION"
 
-mkdir -p build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian
+mkdir -p build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian
 git tag --sort=-authordate \
     --format "$INPUT_PACKAGE_NAME (%(refname:lstrip=-1)) UNRELEASED; urgency=medium%0a%0a  * %(subject)%0a%0a -- %(authorname) %(authoremail)  %(authordate:rfc2822)" \
-    > build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/changelog
-echo 9 > build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/compat
-cp -r debian/* build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/
-sed -i "s/\$VERSION/$VERSION/g" build/"$INPUT_PACKAGE_NAME"-"$VERSION"/debian/control
-cd build/"$INPUT_PACKAGE_NAME"-"$VERSION"
+    > build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/changelog
+echo 9 > build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/compat
+cp -r debian/* build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/
+sed -i "s/\$INPUT_VERSION/$INPUT_VERSION/g" build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/control
+cd build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"
 if [[ "$INPUT_TARGET_ARCHITECTURE" == "amd64" ]]; then
   dpkg-buildpackage
 elif [[ "$INPUT_TARGET_ARCHITECTURE" == "arm64" ]]; then
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
 build:
 	docker build . -t albeego/deb-builder-action:0.0.1
+	docker build . -t albeego/deb-builder-action:latest
 push:
 	docker push albeego/deb-builder-action:0.0.1
+	docker push albeego/deb-builder-action:latest
