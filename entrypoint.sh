40fe0fa31694fc29931cfbae649187594a86fc8f
Steven Leadbeater <info@ledsysconsultancy.com>
2021-02-09 11:02:00 +0000

    Remove problematic build folder clear down

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 cd "$INPUT_EXECUTION_PATH"
 
 echo "Latest version: $INPUT_VERSION"
-rm -rf build/*
 mkdir -p build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian
 git tag --list "$INPUT_TAG_FILTER" --sort=-authordate --sort=-refname \
     --format "$INPUT_PACKAGE_NAME (%(refname:lstrip=-1)) UNRELEASED; urgency=medium%0a%0a  * %(subject)%0a%0a -- %(authorname) %(authoremail)  %(authordate:rfc2822)" \
