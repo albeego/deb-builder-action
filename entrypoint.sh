c6bfc81c74cece32e365f118bf721ef29ff1b710
Steven Leadbeater <info@ledsysconsultancy.com>
2021-02-09 09:04:02 +0000

    Multibuild archs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 cd "$INPUT_EXECUTION_PATH"
 
 echo "Latest version: $INPUT_VERSION"
-
+rm -rf build/*
 mkdir -p build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian
 git tag --list "$INPUT_TAG_FILTER" --sort=-authordate --sort=-refname \
     --format "$INPUT_PACKAGE_NAME (%(refname:lstrip=-1)) UNRELEASED; urgency=medium%0a%0a  * %(subject)%0a%0a -- %(authorname) %(authoremail)  %(authordate:rfc2822)" \
@@ -13,8 +13,8 @@ echo 9 > build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/compat
 cp -r debian/* build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/
 sed -i "s/\$INPUT_VERSION/$INPUT_VERSION/g" build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"/debian/control
 cd build/"$INPUT_PACKAGE_NAME"-"$INPUT_VERSION"
-if [[ "$INPUT_TARGET_ARCHITECTURE" == "amd64" ]]; then
+if [[ "$INPUT_TARGET_ARCHITECTURE" == *"amd64"* ]]; then
   dpkg-buildpackage
-elif [[ "$INPUT_TARGET_ARCHITECTURE" == "arm64" ]]; then
+elif [[ "$INPUT_TARGET_ARCHITECTURE" == *"arm64"* ]]; then
   CONFIG_SITE=/etc/dpkg-cross/cross-config.amd64 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -aarm64 -Pcross,nocheck
 fi
